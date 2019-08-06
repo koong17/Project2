f344450b3b7a4936b83cd1c3e23cc19f79385076
HyesooMoon <it_hyesoo@naver.com>
2019-08-06 15:41:18 +0900

    검색 기능 추가 전
    정렬 미포함

---
+++ hotel/WebContent/js/hidden.js
@@ -0,0 +1,9 @@
+ //보이기
+ function div_show() {
+  document.getElementById("test_div").style.display = "block";
+ }
+ 
+ //숨기기
+ function div_hide() {
+  document.getElementById("test_div").style.display = "none";
+ }
