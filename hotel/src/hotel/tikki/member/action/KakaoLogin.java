c3c2e2967fae0d46f82ecb02752ba78ff0f9c976
황인호 <45850018+hih9484@users.noreply.github.com>
2019-08-14 12:01:39 +0900

    finish

--- hotel/src/hotel/tikki/member/action/KakaoLogin.java
+++ hotel/src/hotel/tikki/member/action/KakaoLogin.java
@@ -15,7 +15,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-public class KakaoAPI {
+public class KakaoLogin {
 
 	public String getAccessToken(String authorize_code) {
 		String access_Token = "";
